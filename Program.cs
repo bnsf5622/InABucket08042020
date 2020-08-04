736ce5a5b0a3df76fb2ac7f0b7f2144c85c27c63
chuck francis <chuck_francis10@yahoo.com>
2020-08-04 10:46:43 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace InABucket08042020
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // You can declare all Kinds of variables.
+            String walrus;
+            double piesEaten;
+            float weightOfTeacupPig;
+            int grainsOfSand;
+
+            // But declaing them just sets up the space for data
+            // to use the variable, you have to put data IN it first!
+            walrus = "Sir Leroy Jenkins III";
+            piesEaten = 42.1;
+
+            Console.WriteLine("Meet my pet Walrus, " + walrus);
+            Console.WriteLine("He was a bit hungry today, and ate this many pies : ");
+            Console.WriteLine(piesEaten);
+        }
+    }
+}
